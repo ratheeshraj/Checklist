c5ab76443192b3a0b207ed0a8926560ee43fe02c
ratheeshraj <58539954+ratheeshraj@users.noreply.github.com>
2023-03-10 23:04:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+let button = document.getElementById('add')
+let todolist= document.getElementById('todolist')
+let input=document.getElementById('input')
+let todos=[]
+
+window.onload=()=>{
+    todos=JSON.parse(localStorage.getItem('todos')) || []
+    todos.forEach(todo=> {
+        addtodo(todo)       
+    });
+}
+
+
+button.addEventListener('click',()=>{
+todos.push(input.value)
+localStorage.setItem('todos',JSON.stringify(todos))
+addtodo(input.value)
+input.value=""
+})
+
+function addtodo(todo){
+        let para = document.createElement('p')
+        para.innerText=todo
+        todoList.appendChild(para)
+        
+
+    para.addEventListener('click',()=>{
+        para.style.textDecoration='line-through'
+        para.style.backgroundColor='yellow'
+        para.style.color='green'
+        remove(todo)
+    })
+    para.addEventListener('dblclick',()=>{
+        todoList.removeChild(para)
+        remove(todo)
+    })
+}
+function remove(todo){
+    let index= todos.indexOf(todo)
+    if(index>-1)
+    todos.splice(index,1)
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
